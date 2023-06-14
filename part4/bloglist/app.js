504eb36603fe8d547dbb6e1fd137ccd46c1b6500
chrisjunlee <chrisjunlee@gmail.com>
2023-06-13 19:54:20 -0700

    Part 4 app: Bloglist. Initial refactoring exercise (4.1 - 4.2)

---
+++ part4/bloglist/app.js
@@ -0,0 +1,27 @@
+const config = require('./utils/config') 
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+const middleWare = require('./utils/middleware')
+
+const blogRouter = require('./controllers/blogRouter')
+
+const mongoUrl = config.MONGODB_URI
+
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+// these middleware must come last
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
