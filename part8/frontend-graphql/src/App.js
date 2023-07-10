aa5e031d5d1ca309b76b471489933fc76c5f5fce
chrisjunlee <chrisjunlee@gmail.com>
2023-07-09 19:54:55 -0700

    GraphQL Subscriptions Frontend: useSubscription and cache update

--- part8/frontend-graphql/src/App.js
+++ part8/frontend-graphql/src/App.js
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import { useState } from "react";
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import LoginForm from './components/LoginForm'
 import Notification from './components/Notification'
-import { useApolloClient } from '@apollo/client'
+import { useApolloClient, useSubscription } from "@apollo/client";
+import { ALL_BOOKS, BOOK_ADDED } from "./queries";
 
 const App = () => {
   const [page, setPage] = useState('login')
@@ -13,6 +14,16 @@ const App = () => {
   // user login management
   const [token, setToken] = useState(null);
   const client = useApolloClient();
+
+  // subscriptions
+  useSubscription(BOOK_ADDED, { 
+    onData: ({ data }) => { 
+      const addedBook = data.data.bookAdded
+      console.log('bookAdded', addedBook)
+      setErrorMessage(`${addedBook.title} added`);
+
+      client.cache.updateQuery({query: ALL_BOOKS}, addedBook)
+    }});
 
   const MONGODB_URI = process.env.MONGODB_URI;
   console.log('MONGODB_URI', MONGODB_URI)
--- part8/frontend-graphql/src/queries.js
+++ part8/frontend-graphql/src/queries.js
@@ -1,5 +1,9 @@
 import {gql} from '@apollo/client'
 
+const BOOK_DETAILS = gql`fragment BookDetails on Book {title author {name} published genres}`
+
 const ALL_BOOKS = gql`query { allBooks { title author {name} published genres } }`
 
-export {ALL_BOOKS}
+const BOOK_ADDED = gql`subscription { bookAdded { ...BookDetails } } ${BOOK_DETAILS}`;
+
+export {ALL_BOOKS, BOOK_ADDED}
