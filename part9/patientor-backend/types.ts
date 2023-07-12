084fd889950ccda33343f6ce9d748349b0ef8724
chrisjunlee <chrisjunlee@gmail.com>
2023-07-11 18:57:40 -0700

    9.12-9.13: Not possible to complete due to material being out of date. There is no way to parse unknown object. Type narrowing does not work

---
+++ part9/patientor-backend/types.ts
@@ -0,0 +1,24 @@
+export interface Diagnosis {
+  code: string;
+  name: string;
+  latin?: string;
+}
+
+export enum Gender {
+  Male = "male",
+  Female = "female",
+  Other = "other"
+}
+
+export interface Patient {
+  id: string;
+  name: string;
+  occupation: string;
+  gender: string;
+  ssn?: string;
+  dateOfBirth?: string;
+}
+
+export type PatientNoSSN = Omit<Patient, 'ssn'>;
+
+export type PatientFormValues = Omit<Patient, "id">;
